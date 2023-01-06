38261b5db3a11824d98aa29b984aa4a567ded7d4
jsmi26 <jenny.smith@seattlechildrens.org>
2023-01-06 11:21:38 -0800

    pvivax expression plots; submit homer analysis to cluster

--- motif_analysis/homer/create_db.sh
+++ motif_analysis/homer/create_db.sh
@@ -5,13 +5,12 @@ set -eou pipefail
 # Set-up environment
 PATH="$HOME/opt/homer/bin:$PATH"
 
-
 # data locations
-PROJDIR="/active/taylor_s/people/jsmi26/RSC/kappe_s_2022.04_PF_LiverStage"
+PROJDIR="/active/taylor_s/people/jsmi26/CP-Bioinformed/kappe_s_2022.04_PF_LiverStage/"
 FASTA="$PROJDIR/species_genomes/plasmoDB/PlasmoDB-58_Pfalciparum3D7_Genome.fasta"
 GTF="$PROJDIR/species_genomes/plasmoDB/PlasmoDB-58_Pfalciparum3D7.gtf"
 
 # Create Pf custom genome DBs
-loadGenome.pl -name "PF3D7" -org null -fasta $FASTA -gtf $GTF -promoters "Pf3D7_promoters" -id custom -force > PF3D7.out
+loadGenome.pl -force -name "PF3D7" -org null -fasta $FASTA -gtf $GTF -promoters "Pf3D7_promoters" -id custom 2> PF3D7.out
 
 # loadGenome.pl -name alf -org null -fasta ALFgenome.fasta -gtf ALFgenes.gtf
