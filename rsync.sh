8fcaa5a6843f65bdde0eb80a6f3df18d83170e7c
jsmi26 <jenny.smith@seattlechildrens.org>
2023-09-22 13:25:24 -0700

    create cvg summary table; update filepaths; generate supp tables

--- rsync.sh
+++ rsync.sh
@@ -2,6 +2,8 @@
 
 destination="/active/kappe_s/kappe/Gigliola/2022.04_jsmi26_PF_LiverStage/"
 
+rsync -av --exclude .DS_Store references/Paper_Versions/Tables/2023.09_Pf_LS_supplemental_tables $destination/Paper_Versions/Tables/
+
 rsync -av --exclude .DS_Store figures $destination 
 rsync -av --exclude .DS_Store samples $destination 
 rsync -av --exclude .DS_Store presentations $destination 
